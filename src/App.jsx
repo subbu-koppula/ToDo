16e6b5e801d84e987141f33f27d9e583038b5e12
saisu <subbu.sai.kopp@gmail.com>
2024-04-04 22:03:59 +0530

    Added media query to change width of body section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import ToDo from './ToDo.jsx'
 import Header from './Header.jsx'
-// import './App.css'
+import './App.css'
 
 function App() {
 
   return(
     <>
       <Header/>
-      <div style={{width : '90%', margin:'auto', marginTop:'1em'}}><ToDo/></div>
+      <div className='mainBody'><ToDo/></div>
     </>
   ) 
 }
