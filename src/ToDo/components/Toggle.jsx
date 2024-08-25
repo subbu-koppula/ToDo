bd64e158a21fb2e9d011e41b17892211e7c956a9
saisu-sai <saikoppulajee@gmail.com>
2024-08-26 01:16:47 +0530

    minor change.

--- src/ToDo/components/Toggle.jsx
+++ src/ToDo/components/Toggle.jsx
@@ -3,6 +3,7 @@ import "./Toggle.css";
 export const Toggle = ({ handleChange, isChecked }) => {
   return (
     <div className="toggle-container">
+      <p style={{color: 'var(--text-color)'}}>Theme:</p>
       <input
         type="checkbox"
         id="check"
